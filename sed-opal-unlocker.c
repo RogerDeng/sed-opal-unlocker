c7ed6eeb2d19f166eb66bc02f23ade12f3ca6120
Michal Gawlik <michal.gawlik@almateria.net>
2018-11-30 21:33:18 +0100

    Command line arguments + parsing them

--- sed-opal-unlocker.c
+++ sed-opal-unlocker.c
@@ -30,15 +30,50 @@ char hash[OPAL_KEY_MAX] = {
 	0x00, 0x00, 0x00, 0x00
 };
 
+void help(const char *banner)
+{
+	if (banner)
+		puts(banner);
+	printf("Usage:\n");
+	printf("\tsed-opal-unlocker <operation> <disk_path> <password_file_path>\n");
+	printf("\n");
+	printf("Where:\n");
+	printf("\t<operation> is one of: lock, unlock, s3save, unlock+s3save\n");
+	printf("\t<disk_path> is device path, ex. /dev/sda, /dev/nvme0n1, etc.\n");
+	printf("\t<password_file_path> is path to file containing the admin1 password\n");
+	printf("\n");
+	printf("Note: when using DTA sedutil-cli to initialize the drive, you need to use its\n");
+	printf("      -n option to disable password hashing, otherwise this utility won't work.\n");
+}
+
 int main(int argc, char* argv[])
 {
-	// Unused variables
-	(void) argc;
-	(void) argv;
-
 	int ret = 0;
+	int mode = -1;
 
-	char *dev = "/dev/nvme0n1";
+	// Parse arguments
+	if (argc < 4)
+		help("Not enough arguments!");
+	else if (strcmp(argv[1], "lock") == 0)
+		mode = 0;
+	else if (strcmp(argv[1], "unlock") == 0)
+		mode = 1;
+	else if (strcmp(argv[1], "s3save") == 0)
+		mode = 2;
+	else if (strcmp(argv[1], "unlock+s3save") == 0)
+		mode = 3;
+	else
+		help("Invalid <operation>!");
+	if (mode < 0)
+		return 0;
+
+	const char *dev = argv[2];
+	const char *passfile = argv[3];
+
+	// Load password
+	// TODO
+
+	// Open the device
 	int fd = open(dev, O_WRONLY);
 	if (fd < 0)
 	{
